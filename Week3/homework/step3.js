8c0979c2bb865d1f28fd0c0e06b2e339f03ebdf9
Ada <adarey80@gmail.com>
2019-02-18 21:22:57 +0100

    First Commit

--- Week3/homework/step3.js
+++ Week3/homework/step3.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function createBase(base) {
-  // Replace this comment and the next line with your code
-  console.log(base);
+  return function(base) {
+    return base + 6;
+  };
 }
 
 const addSix = createBase(6);
